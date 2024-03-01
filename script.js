bf7175b28cbe125b9dee6a6ffac2e282bdc1036b
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-03-01 21:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //your JS code here. If required.
-function allMethods() {
+//function allMethods() {
   //write your code here
+//}
+
+//alert(allMethods());
+function allMethods() {
+    const methodNames = [
+        "abs", "acos", "acosh", "asin", "asinh", "atan", "atan2", "atanh",
+        "cbrt", "ceil", "clz32", "cos", "cosh", "exp", "expm1", "floor",
+        "fround", "hypot", "imul", "log", "log10", "log1p", "log2", "max",
+        "min", "pow", "random", "round", "sign", "sin", "sinh", "sqrt",
+        "tan", "tanh", "trunc"
+    ];
+
+    return methodNames.join(", ");
 }
 
-alert(allMethods());
+console.log(allMethods());
